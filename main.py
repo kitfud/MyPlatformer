e31dd18ac5d8c06a152fd2d00459bff5d3b345c5
Kit Fuderich <kitfuderich@gmail.com>
2021-01-22 16:59:29 +0000

    Player class created and put onto screen

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import pygame
 from pygame.locals import*
 from platforms import Platforms
-
+from player import Player
 import random
 import sys
 
 
+player = ''
+def get_player_actions():
+    p1_actions = {}
+    # create a dictionary of all player images
+    p1_actions["p1_jump"] = pygame.image.load("images/p1_jump.png").convert()
+    p1_actions["p1_jump"].set_colorkey((   0,   0,   0))
+    p1_actions["p1_hurt"] = pygame.image.load("images/p1_hurt.png").convert()
+    p1_actions["p1_hurt"].set_colorkey((0, 0, 0))
+    return p1_actions
 
 pygame.init()
 screen_info = pygame.display.Info()
@@ -18,15 +27,18 @@ color = (255,224,179)
 sprite_list = pygame.sprite.Group()
 platforms = pygame.sprite.Group()
 
-def init():
+def init(p1_actions):
   for i in range(height//100):
     for j in range(width//420):
       plat = Platforms( (random.randint(5, (width - 50) // 10)* 10, 120 * i), 'images/grassHalf.png',70, 40  )
       platforms.add(plat)
+  player = Player((platforms.sprites()[-1].rect.centerx, platforms.sprites()[-1].rect.centery-300), p1_actions)
+  sprite_list.add(player)
 
 def main():
   game_over = False
-  init()
+  p1_actions = get_player_actions()
+  init(p1_actions)
   while True:
     clock.tick(60)
     for event in pygame.event.get():
