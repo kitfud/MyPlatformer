55a7552af3e2c5c24dd79a6119b357de59692fc0
Kit Fuderich <kitfuderich@gmail.com>
2021-01-22 17:27:20 +0000

    still working on movement

--- main.py
+++ main.py
@@ -6,15 +6,8 @@ import random
 import sys
 
 
-player = ''
-def get_player_actions():
-    p1_actions = {}
-    # create a dictionary of all player images
-    p1_actions["p1_jump"] = pygame.image.load("images/p1_jump.png").convert()
-    p1_actions["p1_jump"].set_colorkey((   0,   0,   0))
-    p1_actions["p1_hurt"] = pygame.image.load("images/p1_hurt.png").convert()
-    p1_actions["p1_hurt"].set_colorkey((0, 0, 0))
-    return p1_actions
+
+
 
 pygame.init()
 screen_info = pygame.display.Info()
@@ -27,15 +20,28 @@ color = (255,224,179)
 sprite_list = pygame.sprite.Group()
 platforms = pygame.sprite.Group()
 
+player = ''
+def get_player_actions():
+    p1_actions = {}
+    # create a dictionary of all player images
+    p1_actions["p1_jump"] = pygame.image.load("images/p1_jump.png").convert()
+    p1_actions["p1_jump"].set_colorkey((   0,   0,   0))
+    p1_actions["p1_hurt"] = pygame.image.load("images/p1_hurt.png").convert()
+    p1_actions["p1_hurt"].set_colorkey((0, 0, 0))
+    return p1_actions
+
 def init(p1_actions):
+  global player
   for i in range(height//100):
     for j in range(width//420):
       plat = Platforms( (random.randint(5, (width - 50) // 10)* 10, 120 * i), 'images/grassHalf.png',70, 40  )
       platforms.add(plat)
   player = Player((platforms.sprites()[-1].rect.centerx, platforms.sprites()[-1].rect.centery-300), p1_actions)
   sprite_list.add(player)
+  
 
 def main():
+  global player
   game_over = False
   p1_actions = get_player_actions()
   init(p1_actions)
@@ -49,6 +55,12 @@ def main():
           pygame.display.set_mode(size,FULLSCREEN)
         if event.key == K_ESCAPE:
           pygame.display.set_mode(size)
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+      player.left()
+    if keys[pygame.K_RIGHT]:
+      player.right()
+      
     screen.fill(color)
     platforms.draw(screen)
     sprite_list.draw(screen)
--- player.py
+++ player.py
@@ -10,15 +10,34 @@ class Player(pygame.sprite.Sprite):
         # index 0 represents dx, index 1 represents dy
         self.xy_speed = pygame.math.Vector2(0, 0)
         self.facing = "R"
+        self.jump_speed = -14
 
-    def update(self):
+    def update(self, platforms):
+        screen_info = pygame.display.Info()
         #update the image and direction
         self.image = self.images['p1_jump']
         if self.facing == "L":
             self.image = pygame.transform.flip(self.image, True,False)
+        
+        # move the player
+        self.rect.move_ip(self.xy_speed)
+        # handle left/right movement
+        self.xy_speed[0] = 0
+        if self.rect.right < 0:
+          self.rect.left = screen_info.current_w
+        elif self.rect.left > screen_info.current_w:
+          self.rect.right = 0   
+        hit_list = pygame.sprite.spritecollide(self, platforms, False)
+        for plat in hit_list:
+          if self.xy_speed[1] > 0 and abs(self.rect.bottom -plat.rect.top <= self.xy_speed[1]):
+            self.rect.bottom = plat.rect.top
+            self.xy_speed[1] = self.jump_speed
+
 
     def left(self):
         self.facing = 'L'
+        self.xy_speed[0] = -6
 
     def right(self):
         self.facing = 'R'
+        self.xy_speed[0] = 6
