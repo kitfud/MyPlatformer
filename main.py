c31efe5d24673504e93cd90c0c347401810c4a93
Kit Fuderich <kitfuderich@gmail.com>
2021-01-23 00:35:14 +0000

    player movements added

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
-import pygame
-from pygame.locals import*
-from platforms import Platforms
-from player import Player
 import random
+import pygame
 import sys
-
-
-
-
+from pygame.locals import *
+from player import Player
+from platforms import Platforms
 
 pygame.init()
 screen_info = pygame.display.Info()
 
-size = (width,height) = (screen_info.current_w,screen_info.current_h)
-
+# set the width and height to the size of the screen
+size = (width, height) = (screen_info.current_w, screen_info.current_h)
+font = pygame.font.SysFont(None, 70)
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-color = (255,224,179)
+color = (255, 224, 179)
+
 sprite_list = pygame.sprite.Group()
 platforms = pygame.sprite.Group()
-
 player = ''
+
+
 def get_player_actions():
     p1_actions = {}
     # create a dictionary of all player images
@@ -30,41 +29,50 @@ def get_player_actions():
     p1_actions["p1_hurt"].set_colorkey((0, 0, 0))
     return p1_actions
 
+
 def init(p1_actions):
-  global player
-  for i in range(height//100):
-    for j in range(width//420):
-      plat = Platforms( (random.randint(5, (width - 50) // 10)* 10, 120 * i), 'images/grassHalf.png',70, 40  )
-      platforms.add(plat)
-  player = Player((platforms.sprites()[-1].rect.centerx, platforms.sprites()[-1].rect.centery-300), p1_actions)
-  sprite_list.add(player)
-  
+    global player
+    for i in range(height // 100):
+        for j in range(width // 420):
+            plat = Platforms((random.randint(5, (width - 50) // 10) * 10,  120 * i), 'images/grassHalf.png', 70, 40)
+            platforms.add(plat)
+    player = Player((platforms.sprites()[-1].rect.centerx, platforms.sprites()[-1].rect.centery-300), p1_actions)
+    sprite_list.add(player)
+
 
 def main():
-  global player
-  game_over = False
-  p1_actions = get_player_actions()
-  init(p1_actions)
-  while True:
-    clock.tick(60)
-    for event in pygame.event.get():
-      if event.type == QUIT:
-        sys.exit()
-      if event.type == KEYDOWN:
-        if event.key == K_f:
-          pygame.display.set_mode(size,FULLSCREEN)
-        if event.key == K_ESCAPE:
-          pygame.display.set_mode(size)
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-      player.left()
-    if keys[pygame.K_RIGHT]:
-      player.right()
-      
-    screen.fill(color)
-    platforms.draw(screen)
-    sprite_list.draw(screen)
-    pygame.display.flip()
+    global player
+    game_over = False
+    # create platforms
+    p1_actions = get_player_actions()
+    init(p1_actions)
+
+    while True:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_f:
+                    pygame.display.set_mode(size, FULLSCREEN)
+                if event.key == K_ESCAPE:
+                    pygame.display.set_mode(size)
+                
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            player.left()
+        if keys[pygame.K_RIGHT]:
+            player.right()
+
+        if player.update(platforms):
+            game_over = True
+    
+        screen.fill(color)
+        platforms.draw(screen)
+        sprite_list.draw(screen)
+       
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
-  main()
+    main()
--- player.py
+++ player.py
@@ -1,6 +1,8 @@
 import pygame
 
+
 class Player(pygame.sprite.Sprite):
+
     def __init__(self, pos, images):
         super().__init__()
         self.images = images
@@ -11,33 +13,36 @@ class Player(pygame.sprite.Sprite):
         self.xy_speed = pygame.math.Vector2(0, 0)
         self.facing = "R"
         self.jump_speed = -14
+        # self.world_y = 0
+        # self.progress = 0
 
     def update(self, platforms):
         screen_info = pygame.display.Info()
-        #update the image and direction
-        self.image = self.images['p1_jump']
-        if self.facing == "L":
-            self.image = pygame.transform.flip(self.image, True,False)
-        
-        # move the player
+
         self.rect.move_ip(self.xy_speed)
         # handle left/right movement
         self.xy_speed[0] = 0
+       
         if self.rect.right < 0:
-          self.rect.left = screen_info.current_w
+            self.rect.left = screen_info.current_w
         elif self.rect.left > screen_info.current_w:
-          self.rect.right = 0   
+            self.rect.right = 0
+
+
+        # check if the player hit any platforms
         hit_list = pygame.sprite.spritecollide(self, platforms, False)
         for plat in hit_list:
-          if self.xy_speed[1] > 0 and abs(self.rect.bottom -plat.rect.top <= self.xy_speed[1]):
-            self.rect.bottom = plat.rect.top
-            self.xy_speed[1] = self.jump_speed
-
+            # player landed on top of a platform
+            if self.xy_speed[1] > 0 and abs(self.rect.bottom - plat.rect.top) <= self.xy_speed[1]:
+                self.rect.bottom = plat.rect.top
+                self.xy_speed[1] = self.jump_speed
+        # gravity
+        self.xy_speed[1] += .5
 
     def left(self):
         self.facing = 'L'
         self.xy_speed[0] = -6
 
     def right(self):
-        self.facing = 'R'
+        self.facing = "R"
         self.xy_speed[0] = 6
